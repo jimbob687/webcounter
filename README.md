14e320af6fd7ad71a966f4f9d3d69e6ef82069be
jimbob687 <jimbob687@users.noreply.github.com>
2022-02-07 20:52:33 -0800

    Feature/build helm charts (#1)
    * Adding READMEE
    
    * Adding helm charts
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: James Nieper <jnieper@siftscience.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+The solution  
+A golang webserver has been created and built into an image which is pushed to docker hub.  
+Thhe image is deployed as a deployment behind a service. By default is set to be a single pod deployed, however the number can be increased.  
+THe golang server will connect to a redis instance behind a service, and will connect to it.  
+
+Steps to build the image  
+1. cd webimage
+2. Build the image `docker build --tag jimbob687/webcounter:latest .`
+3. Push image to registry `docker push jimbob687/webcounter:latest`
+
+Steps to deploy the chart  
+1. cd webcounter  
+2. Create namespace `kubectl create namespace webcounter`  
+3. Install the helm chart `helm install webcounter . -n webcounter`  
+4. Expose node port `minikube service --url webcounter -n webcounter`  
+5. Take the output url from step 4 and enter into a browser  
--- webimage/main.go
+++ webimage/main.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/go-redis/redis/v8"
-    "strconv"
-    "log"
-    "net/http"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 var ctx = context.Background()
 
 func main() {
 
-    //http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) {
-    //    fmt.Println("in handfunc")
-    //    counterVal := webCounter()
-    //    fmt.Fprintf(w, "Holla! we have hit %s times", counterVal)
-    //})
+	http.HandleFunc("/", webCounter)
 
-    http.HandleFunc("/", webCounter)
-
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func webCounter(w http.ResponseWriter, r *http.Request) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+	if r.URL.Path == "/" {
+		redisUrl := os.Getenv("REDIS_URL")
+		redisAddr := fmt.Sprintf("%s:6379", redisUrl)
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     redisAddr,
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
 
-	val, err := rdb.Get(ctx, "key1").Result()
-	if err != nil {
-		fmt.Println("key1 not found")
-		storeValue(rdb, "1")
-	} else {
-		intVar, err := strconv.Atoi(val)
-		fmt.Println("Got value: ", val)
+		val, err := rdb.Get(ctx, "key1").Result()
 		if err != nil {
-			fmt.Println("error: ", err)
+			fmt.Println("key1 not found")
+			storeValue(rdb, "1")
+                        val = "1"
 		} else {
-			intVar++
-			storeValue(rdb, strconv.Itoa(intVar))
+			intVar, err := strconv.Atoi(val)
+			fmt.Println("Got value: ", val)
+			if err != nil {
+				fmt.Println("error: ", err)
+			} else {
+				intVar++
+				storeValue(rdb, strconv.Itoa(intVar))
+			}
 		}
-	}
 
-    fmt.Fprintf(w, "Holla! we have hit %s times", val)
+		fmt.Fprintf(w, "Holla! we have hit %s times", val)
+	}
 }
 
 func storeValue(rdb *redis.Client, value string) {
-    fmt.Println("About to store value")
+	fmt.Println("About to store value")
 	err := rdb.Set(ctx, "key1", value, 0).Err()
 	if err != nil {
 		fmt.Println("Unable to store value")
